e429d8d17d9354e592e7c7166fd90a90e8a4fcf0
EmmanuelIdeho <ohis98@gmail.com>
2024-12-28 18:10:57 +0000

    Started Group and reformat data in df_cat

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -7,19 +7,26 @@ import numpy as np
 df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = ((df['weight'] / df['height']**2) > 25).astype(int)
+df['overweight'] = df['weight'] / (df['height']**2)
 
 # 3
+df.loc[df['overweight'] <= 25, 'overweight'] = 0
+df.loc[df['overweight'] > 25, 'overweight'] = 1
+
+df.loc[df['cholesterol'] == 1, 'cholesterol'] = 0
+df.loc[df['cholesterol'] > 1, 'cholesterol'] = 1
+df.loc[df['gluc'] == 1, 'gluc'] = 0
+df.loc[df['gluc'] > 1, 'gluc'] = 1
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=["cardio"], value_vars=["cholesterol","gluc","smoke","alco","active","overweight"])
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby('cardio')
     
 
     # 7
